34e658ec8ef3864795eed6f87000bc8aaf493e49
dlalstmd <minid0329@gmail.com>
2022-09-21 18:17:46 +0900

    feat : request code

--- app/src/main/java/com/example/test3/RegisterRequest.java
+++ app/src/main/java/com/example/test3/RegisterRequest.java
@@ -2,14 +2,28 @@ package com.example.test3;
 
 import androidx.annotation.Nullable;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
-public class RegisterRequest extends StringRequest{
-    final static private String URL = "minid0329.ivyro.net";
+import java.util.HashMap;
 
-    public RegisterRequest(int method, String url, Response.Listener<String> listener, @Nullable Response.ErrorListener errorListener) {
-        super(method, url, listener, errorListener);
+public class RegisterRequest extends StringRequest{
+    final static private String URL = "http://minid0329.ivyro.net/public_html/register.php";
+    private HashMap<String,String> map;
+
+    public RegisterRequest(String userID, String userPW, String userName, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("userID", userID);
+        map.put("userPW", userPW);
+        map.put("userName", userName);
+
+    }
+    @Override
+    protected HashMap<String, String> getParams() throws AuthFailureError {
+        return map;
     }
 
     public RegisterRequest(String url, Response.Listener<String> listener, @Nullable Response.ErrorListener errorListener) {
